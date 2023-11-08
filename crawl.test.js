a7158bcdacaea492e2e6f508aad3a55f4b9388f4
faisalnazir7 <bhatfaisalfnb@gmail.com>
2023-11-08 23:32:15 +0530

    CREATE: test >> getURLsFromHTML absolute

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require("./crawl.js")
+const {normalizeURL, getURLsFromHTML} = require("./crawl.js")
 const {test, expect} = require("@jest/globals")
 
 test("normalizeURL strip protocol", () => {
@@ -26,5 +26,21 @@ test("normalizeURL strip http", () => {
     const input = 'http://faisalnazir.tech/about'
     const actual = normalizeURL(input)
     const expected = 'faisalnazir.tech/about'
+    expect(actual).toEqual(expected)
+})
+
+test("getURLsFromHTML absolute", () => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href="https://faisalnazir.tech/">
+                Faisal Nazir
+            </a>    
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://faisalnazir.tech"
+    const actual = getURLsFromHTML(inputHTML, inputBaseURL)
+    const expected = ["https://faisalnazir.tech/"]
     expect(actual).toEqual(expected)
 })
