5a8b856e71b59f714061b11090605e12b4a3f916
faisalnazir7 <bhatfaisalfnb@gmail.com>
2023-11-12 14:23:07 +0530

    CREATE: main >> init >> entry-point function

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("good to go!")
+function main() {
+    if(process.argv.length < 3) {
+        console.log("No website provided")
+        process.exit(1)
+    }
+    
+    if(process.argv.length > 3) {
+        console.log("Too many arguments provided")
+        process.exit(1)
+    }
+
+    const baseURL = process.argv[2]
+
+    console.log(`Starting crawling ${baseURL}`)
+}
+
+main()
